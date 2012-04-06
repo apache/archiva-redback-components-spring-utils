16cf40047474edf6ef4a90ae947aa4309bcbf6d2
Olivier Lamy <olamy@apache.org>
2012-04-06 21:33:42 +0000

    fix license headers
    git-svn-id: https://svn.apache.org/repos/asf/archiva/redback/redback-components/trunk@1310599 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/codehaus/redback/components/springutils/CachingWebApplicationContext.java
+++ src/main/java/org/codehaus/redback/components/springutils/CachingWebApplicationContext.java
@@ -9,7 +9,7 @@ package org.codehaus.redback.components.springutils;
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -18,7 +18,6 @@ package org.codehaus.redback.components.springutils;
  * specific language governing permissions and limitations
  * under the License.
  */
-
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.web.context.support.XmlWebApplicationContext;
 
